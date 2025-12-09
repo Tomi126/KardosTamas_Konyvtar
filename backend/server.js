317790403ce62493c60e76bdbd651d320380a080
Tomi126 <115153488+Tomi126@users.noreply.github.com>
2025-12-09 14:39:37 +0100

    backend

--- backend/server.js
+++ backend/server.js
@@ -23,13 +23,36 @@ app.get("/konyvek", (req, res) => {
     const sql = "SELECT konyv_id, cim, szerzo, mufaj_nev FROM konyv INNER JOIN mufaj ON konyv.mufaj_id = mufaj.mufaj_id;";
     db.query(sql, (err, result) => {
         if (err) {
-            console.error("Error fetching books:", err);
-            res.status(500).send("Error fetching books");
+            res.status(500).send("Hiba a könyvek lekérése során");
         } else {
             res.json(result);
         }});
 });
 
+app.post("/ujkonyv", express.json() ,(req, res) => {
+    const konyv = req.body;
+    const sql = `INSERT INTO konyv (konyv_id, cim, szerzo, mufaj_id) VALUES (?, ?, ?, ?);`;
+
+    db.query(sql, [konyv.konyv_id, konyv.cim, konyv.szerzo, konyv.mufaj_id], (err, result) => {
+        if (err) {
+            res.status(500).send("Hiba az új könyv hozzáadása során");
+        } else {
+            res.send("Könyv hozzáadva sikeresen");
+        }
+    });
+});
+
+app.delete("/konyvtorles/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "DELETE FROM konyv WHERE konyv_id = ?;";
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            res.status(500).send("Hiba a könyv törlése során");
+        } else {
+            res.send("Könyv törölve sikeresen");
+        }
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
